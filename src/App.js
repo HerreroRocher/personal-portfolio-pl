b79bf489fc0d6d14cd4d66bb9b8a0df7f3b1cf4f
Daniel Herrero <danielherrero4465@gmail.com>
2025-10-21 00:47:16 +0100

    Refactored Education

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import Education from './Education';
 import educationImg from './education_img.jpg'; // Ensure you have this image in the src folder
 import projectsImg from './projects_img.jpg'; // Ensure you have this image in the src folder
 import experienceImg from './experience_img.jpg'; // Ensure you have this image in the src folder
@@ -9,73 +10,9 @@ import languagesImg from './languages_img.jpg'; // Ensure you have this image in
 import homeProjectsImg from './home_projects_img.jpg'; // Ensure you have this image in the src folder
 import schoolProjectsImg from './school_projects_img.jpeg'; // Ensure you have this image in the src folder
 
-function EducationInfo() {
-  return (
-    <>
-    <Uni />
-    <A_Levels />
-    <GCSEs />
-    </>
-  );
-}
 
-function Uni(){
-  return (
-    <div className="information">
-      <h4>The University of Manchester</h4>
-      <p>Location: Manchester, UK</p>
-      <p>Degree: BSc with Honours in Computer Science</p>
-      <p>Duration: Sep 2023 - Jul 2026</p>
-      <p>• Expected graduation in 2026</p>
-      <p>• Expected first class degree</p>
-      <p>
-        • Achieved relevant course units: Logic and Modelling(80%), Software
-        Engineering(70%), Databases(72%), Machine Learning(67%), Distributed
-        Systems(66%), Algorithms and Data Structures(72%), Data Science(75%), First Year Team Project(78%)
-      </p>
-      <p>
-        • Current relevant course units: Third Year Project Laboratory, AI and Games,
-        Natural Language Processing & Understanding, Advanced Distributed Systems, Agile
-        Software Pipelines
-      </p>
-    </div>
-  );
-}
 
-function A_Levels(){
-  return (
-    <div className="information">
-    <h4>Finchley Catholic High School</h4>
-    <p>Location: London, UK</p>
-    <p>Duration: Sep 2021 ‑ Jun 2023</p>
-    <h5>A‑Levels:</h5>
-    <p>• Physics - A*</p>
-    <p>• Mathematics - A*</p>
-    <p>• Computer Science - A</p>
-    </div>
-  )
-}
 
-function GCSEs(){
-  return (
-    <div className="information">
-    <h4>Finchley Catholic High School</h4>
-    <p>Location: London, UK</p>
-    <p>Duration: Sep 2018 ‑ Jun 2021</p>
-    <h5>GCSEs:</h5>
-    <p>• Business - 9</p>
-    <p>• Biology - 9</p>
-    <p>• Physics - 9</p>
-    <p>• Mathematics - 9</p>
-    <p>• French - 8</p>
-    <p>• Chemistry - 8</p>
-    <p>• Computer Science - 7</p>
-    <p>• English Literature - 7</p>
-    <p>• English Language - 6</p>
-    <p>• Religious Studies - 6</p>
-    </div>
-  )
-}
 
 function EmployeeManagementSystem() {
   return (
@@ -474,7 +411,7 @@ function Section({ title, imageUrl }) {
   const renderContent = () => {
     switch (title) {
       case 'Education':
-        return <EducationInfo />;
+        return <Education />;
       case 'Projects':
         return (
           <>
---
+++ src/Education.js
@@ -0,0 +1,67 @@
+
+export default function Education() {
+  return (
+    <>
+      <Uni />
+      <ALevels />
+      <GCSEs />
+    </>
+  );
+}
+
+function Uni() {
+  return (
+    <div className="information">
+      <h4>The University of Manchester</h4>
+      <p>Location: Manchester, UK</p>
+      <p>Degree: BSc with Honours in Computer Science</p>
+      <p>Duration: Sep 2023 - Jul 2026</p>
+      <p>• Expected graduation in 2026</p>
+      <p>• Expected first class degree</p>
+      <p>• Achieved relevant course units: Logic and Modelling(80%), Software
+        Engineering(70%), Databases(72%), Machine Learning(67%), Distributed
+        Systems(66%), Algorithms and Data Structures(72%), Data Science(75%),
+        First Year Team Project(78%)
+      </p>
+      <p>• Current relevant course units: Third Year Project Laboratory, AI and
+        Games, Natural Language Processing & Understanding, Advanced Distributed
+        Systems, Agile Software Pipelines
+      </p>
+    </div>
+  );
+}
+
+function ALevels() {
+  return (
+    <div className="information">
+      <h4>Finchley Catholic High School</h4>
+      <p>Location: London, UK</p>
+      <p>Duration: Sep 2021 ‑ Jun 2023</p>
+      <h5>A‑Levels:</h5>
+      <p>• Physics - A*</p>
+      <p>• Mathematics - A*</p>
+      <p>• Computer Science - A</p>
+    </div>
+  );
+}
+
+function GCSEs() {
+  return (
+    <div className="information">
+      <h4>Finchley Catholic High School</h4>
+      <p>Location: London, UK</p>
+      <p>Duration: Sep 2018 ‑ Jun 2021</p>
+      <h5>GCSEs:</h5>
+      <p>• Business - 9</p>
+      <p>• Biology - 9</p>
+      <p>• Physics - 9</p>
+      <p>• Mathematics - 9</p>
+      <p>• French - 8</p>
+      <p>• Chemistry - 8</p>
+      <p>• Computer Science - 7</p>
+      <p>• English Literature - 7</p>
+      <p>• English Language - 6</p>
+      <p>• Religious Studies - 6</p>
+    </div>
+  );
+}
